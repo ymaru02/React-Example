7b10ef835fcf16a83ff6cd50870ea16f43d693d1
Ymaru02 <dyun022@gmail.com>
2022-03-17 10:44:54 +0900

    arrangement

---
+++ Fastcampus/React_foundation/my-app/src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// import Condition from "./components/2-8.ConditionalRendering/Condition";
+// import Compositions from "./components/2-4.Props/Compositions";
+// import Extaction from "./components/2-4.Props/Extraction/Extaction";
+// import ClassComponent from "./components/2-5.State/ClassComponent";
+// import FunctionalComponent from "./components/2-5.State/FunctionalComponent";
+// import Compositions2 from "./components/2-6.LifeCycle/ClassComponent";
+// import Event from "./components/2-7.Event/Event";
+// import List from "./components/2-9.List/List";
+// import ControlledComponent from "./components/2-10.Form/ControlledComponent";
+// import UncontrolledComponent from "./components/2-10.Form/UncontrolledComponent";
+
+// import Input from "./3-7.HOC/Input";
+// import Button from "./3-7.HOC/Button";
+// import Memo from "./3-8.Memoization/Memo";
+// import Example from "./3-10.Context/Example";
+// import Example from "./3-11.Portal/Example";
+// import Component from "./3-14.PropTypes/Component";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
